b3a7930991ec792dfda63771bf10ff8162192908
Esteban Gomez <unawaretub86@gmail.com>
2022-05-02 21:21:38 -0500

    First push

---
+++ src/main/java/com/example/demo/PracticaFutbolApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaFutbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaFutbolApplication.class, args);
+	}
+
+}
